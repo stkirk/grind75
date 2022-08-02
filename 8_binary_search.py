fca56dab779bc317ccfeec4e93624a3ea6114109
stkirk <shaanekirk@gmail.com>
2022-08-02 15:40:34 -0400

    solution for binary search and added next problem file

--- 8_binary_search.py
+++ 8_binary_search.py
@@ -2,8 +2,28 @@
 # if target exists in nums return its index, otherwise -1
 # nums is sorted in ascending order and unique
 
+# solution must have O(log n) time complexity
+
 def binary_search(nums, target):
-    return nums
+    # init pointers for left, right bounds
+    right = len(nums) - 1
+    left = 0
+
+    while left <= right:
+        guess = (right + left) // 2
+
+        if nums[guess] == target:
+            return guess
+
+        elif nums[guess] > target:
+            # move right bound to guess_idx - 1
+            right = guess - 1
+        # else - guess < target, move left boundary to guess_idx + 1
+        else:
+            left = guess + 1
+
+    # loop finished without finding target
+    return -1
 
 print(binary_search([-1,0,3,5,9,12], 9)) # 4
 print(binary_search([-1,0,3,5,9,12], 2)) # -1

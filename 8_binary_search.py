64dad0c8f2dd4ae0d10c86943b02c1a7cfdaa0b5
stkirk <shaanekirk@gmail.com>
2022-08-02 14:56:52 -0400

    moved files to main dir

---
+++ 8_binary_search.py
@@ -0,0 +1,9 @@
+# Given an array of ints, nums, and target:
+# if target exists in nums return its index, otherwise -1
+# nums is sorted in ascending order and unique
+
+def binary_search(nums, target):
+    return nums
+
+print(binary_search([-1,0,3,5,9,12], 9)) # 4
+print(binary_search([-1,0,3,5,9,12], 2)) # -1
